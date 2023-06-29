1bffe5a308b3c6ab493886d3f811f81604fd78c3
uchida <mamimami49192@gmail.com>
2023-06-29 13:39:11 +0900

    3-5 IconButtonの学習

--- lib/main.dart
+++ lib/main.dart
@@ -9,84 +9,79 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
       home: Scaffold(
         body: Column(
           children: <Widget>[
             Container(
               padding: const EdgeInsets.only(top: 32),
-              child: const Text('TextButton'),
+              child: const Text('IconButton'),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                const TextButton(
-                  onPressed: null,
-                  child: Text('disabled'),
-                ),
-                TextButton(
+                IconButton(
                   onPressed: () {},
-                  child: const Text('enabled'),
+                  icon: const Icon(Icons.thumb_up),
                 ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.orange,
-                  ),
+                IconButton(
+                  color: Colors.green,
                   onPressed: () {},
-                  child: const Text('enabled'),
+                  icon: const Icon(Icons.favorite),
+                ),
+                IconButton(
+                  iconSize: 64,
+                  onPressed: () {},
+                  icon: const Icon(Icons.flight),
                 ),
               ],
             ),
             Container(
-              padding: const EdgeInsets.only(top: 90),
-              child: const Text('OutlinedButton'),
+              padding: const EdgeInsets.only(top: 100),
+              child: const Text('アイコン＋テキスト'),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                const OutlinedButton(
-                  onPressed: null,
-                  child: Text('disabled'),
-                ),
-                OutlinedButton(
+                TextButton.icon(
                   onPressed: () {},
-                  child: const Text('enabled'),
+                  icon: const Icon(Icons.thumb_up),
+                  label: const Text('Good'),
                 ),
-                OutlinedButton(
+                OutlinedButton.icon(
                   onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Colors.orange,
-                  ),
-                  child: const Text('enabled'),
+                  icon: const Icon(Icons.favorite, color: Colors.orange),
+                  label: const Text('Like'),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.flight),
+                  label: const Text('Flight'),
                 ),
               ],
             ),
             Container(
-              padding: const EdgeInsets.only(top: 90),
-              child: const Text('ElevatedButton'),
+              padding: const EdgeInsets.only(top: 100),
+              child: const Text('アイコン＋テキスト②練習用'),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                const ElevatedButton(
-                  onPressed: null,
-                  child: Text('disabled'),
-                ),
-                ElevatedButton(
+                TextButton.icon(
                   onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    elevation: 7,
-                  ),
-                  child: const Text('enabled'),
+                  icon: const Icon(Icons.filter_drama, color: Colors.blue),
+                  label: const Text('cloudy'),
                 ),
-                ElevatedButton(
+                OutlinedButton.icon(
                   onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.orange,
-                    elevation: 12,
-                  ),
-                  child: const Text('enabled'),
+                  icon: const Icon(Icons.abc),
+                  label: const Text('ABC'),
                 ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.dangerous),
+                  label: const Text('Danger'),
+              ),
               ],
             ),
           ],

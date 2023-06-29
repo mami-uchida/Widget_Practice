8397885241979376ce4ac048887c638f20fdfc47
uchida <mamimami49192@gmail.com>
2023-06-29 13:54:01 +0900

    3-5 FloatingActionButtonの学習

--- lib/main.dart
+++ lib/main.dart
@@ -9,82 +9,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-              child: const Text('IconButton'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.thumb_up),
-                ),
-                IconButton(
-                  color: Colors.green,
-                  onPressed: () {},
-                  icon: const Icon(Icons.favorite),
-                ),
-                IconButton(
-                  iconSize: 64,
-                  onPressed: () {},
-                  icon: const Icon(Icons.flight),
-                ),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 100),
-              child: const Text('アイコン＋テキスト'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.thumb_up),
-                  label: const Text('Good'),
-                ),
-                OutlinedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.favorite, color: Colors.orange),
-                  label: const Text('Like'),
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.flight),
-                  label: const Text('Flight'),
-                ),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 100),
-              child: const Text('アイコン＋テキスト②練習用'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.filter_drama, color: Colors.blue),
-                  label: const Text('cloudy'),
-                ),
-                OutlinedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.abc),
-                  label: const Text('ABC'),
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.dangerous),
-                  label: const Text('Danger'),
-              ),
-              ],
-            ),
-          ],
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () {},
+          child: const Icon(Icons.thumb_up),
         ),
       ),
     );

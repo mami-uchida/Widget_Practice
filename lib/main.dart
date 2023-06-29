b0cde48c3afa12ea422ecb62dc55cfec1dae237c
uchida <mamimami49192@gmail.com>
2023-06-29 16:15:51 +0900

    3-6 リストビューの学習

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final List<Map<String, dynamic>> listItems = [
+    {
+      'text': 'Item 1',
+      'color': Colors.blue[600],
+    },
+    {
+      'text': 'Item 2',
+      'color': Colors.blue[300],
+    },
+    {
+      'text': 'Item 3',
+      'color': Colors.blue[100],
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +24,66 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.red,
-          onPressed: () {},
-          child: const Icon(Icons.thumb_up),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 125,
+              padding: const EdgeInsets.all(4),
+              // childrenを指定してリスト表示
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    color: Colors.blue[600],
+                    child: Text('Item 1'),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.blue[300],
+                    child: Text('Item 2'),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.blue[100],
+                    child: Text('Item 3'),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 70,
+              padding: EdgeInsets.all(4),
+              // 配列を元にリスト表示
+              child: ListView.builder(
+                itemCount: listItems.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 50,
+                    color: listItems[index]['color'],
+                    child: Text(listItems[index]['text']),
+                  );
+                },
+              ),
+            ),
+            Container(
+              height: 250,
+              padding: EdgeInsets.all(30),
+              // 各アイテムの間にスペースなどを挟みたい場合
+              child: ListView.separated(
+                itemCount: listItems.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 100,
+                    color: listItems[index]['color'],
+                    child: Text(listItems[index]['text']),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider();
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );

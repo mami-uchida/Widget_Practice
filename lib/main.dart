6421f33f509f1f81b357559fe717adc91e220107
uchida <mamimami49192@gmail.com>
2023-06-29 17:25:01 +0900

    3-6 ListTile・Cardの学習

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<Map<String, dynamic>> listItems = [
-    {
-      'text': 'Item 1',
-      'color': Colors.blue[600],
-    },
-    {
-      'text': 'Item 2',
-      'color': Colors.blue[300],
-    },
-    {
-      'text': 'Item 3',
-      'color': Colors.blue[100],
-    },
-  ];
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,61 +13,28 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Column(
           children: <Widget>[
-            Container(
-              height: 125,
-              padding: const EdgeInsets.all(4),
-              // childrenを指定してリスト表示
-              child: ListView(
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    color: Colors.blue[600],
-                    child: Text('Item 1'),
-                  ),
-                  Container(
-                    height: 50,
-                    color: Colors.blue[300],
-                    child: Text('Item 2'),
-                  ),
-                  Container(
-                    height: 50,
-                    color: Colors.blue[100],
-                    child: Text('Item 3'),
-                  ),
-                ],
+            // タイトル・サブタイトル・画像・アイコン等を含めたアイテムが作れる
+            ListTile(
+              leading: Image.network('https://placehold.jp/50x50.png'),
+              title: const Text('ListTile'),
+              subtitle: const Text('subtitle'),
+              trailing: const Icon(Icons.more_vert),
+            ),
+            // 影のついたカードUIが作れる
+            Card(
+              child: Container(
+                height: 60,
+                width: double.infinity,
+                child: const Text('Card'),
               ),
             ),
-            Container(
-              height: 70,
-              padding: EdgeInsets.all(4),
-              // 配列を元にリスト表示
-              child: ListView.builder(
-                itemCount: listItems.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 50,
-                    color: listItems[index]['color'],
-                    child: Text(listItems[index]['text']),
-                  );
-                },
-              ),
-            ),
-            Container(
-              height: 250,
-              padding: EdgeInsets.all(30),
-              // 各アイテムの間にスペースなどを挟みたい場合
-              child: ListView.separated(
-                itemCount: listItems.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 100,
-                    color: listItems[index]['color'],
-                    child: Text(listItems[index]['text']),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return Divider();
-                },
+            // 組み合わせることもOK
+            Card(
+              child: ListTile(
+                leading: Image.network('https://placehold.jp/50x50.png'),
+                title: const Text('Card and ListTile'),
+                subtitle: const Text('subtitle'),
+                trailing: const Icon(Icons.more_vert),
               ),
             ),
           ],
